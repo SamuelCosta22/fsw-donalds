d4a41190d1e5b8c25487224032ea0c9d515c53e1
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-02-26 10:03:16 -0300

    feat: add scroll to product details page

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { Prisma } from "@prisma/client";
-import { ChefHatIcon, MinusIcon, PlusIcon } from "lucide-react";
+import { ChefHatIcon, InfoIcon, MinusIcon, PlusIcon } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/utils/format-currency";
 
 interface ProductDetailsProps {
@@ -28,8 +29,8 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
     }
     
     return (
-        <div className="relative z-50 rounded-t-3xl p-5 mt-[-1rem] flex-auto flex flex-col">
-            <div className="flex-auto">
+        <div className="relative z-50 rounded-t-3xl p-5 mt-[-1rem] flex-auto flex flex-col overflow-hidden">
+            <div className="flex-auto overflow-hidden">
                 <div className="items-center flex gap-1.5">
                     <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full" />
                     <p className="text-xs text-muted-foreground">{product.restaurant.name}</p>
@@ -37,7 +38,7 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
 
                 <h2 className="mt-1 text-lg font-semibold">{product.name}</h2>
 
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between mt-1">
                     <h3 className="text-lg font-semibold">{formatCurrency(product.price)}</h3>
                     <div className="flex items-center gap-3 text-center">
                         <Button variant="outline" className="h-8 w-8 rounded-xl" onClick={handleDecreaseQuantity}><MinusIcon /></Button>
@@ -47,18 +48,27 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
                     </div>
                 </div>
 
-                <div className="mt-5 space-y-2">
-                    <h4 className="font-semibold">Sobre</h4>
-                    <p className="text-xs text-muted-foreground text-justify">{product.description}</p>
-                </div>
-
-                <div className="mt-5 space-y-2">
-                    <div className="flex items-center gap-1">
-                        <ChefHatIcon size={18} />
-                        <h4 className="font-semibold">Ingredientes</h4>
+                <ScrollArea className="w-full max-h-64 overflow-auto mt-2">
+                    <div className="mt-5 space-y-2">
+                        <div className="flex items-center gap-1">
+                            <InfoIcon size={18} />
+                            <h4 className="font-semibold">Sobre</h4>
+                        </div>
+                        <p className="text-xs text-muted-foreground text-justify">{product.description}</p>
                     </div>
-                    <p className="text-xs text-muted-foreground text-justify">{product.ingredients}</p>
-                </div>
+
+                    <div className="mt-5 space-y-2">
+                        <div className="flex items-center gap-1">
+                            <ChefHatIcon size={18} />
+                            <h4 className="font-semibold">Ingredientes</h4>
+                        </div>
+                        <ul className="list-disc px-5 text-xs text-muted-foreground text-justify">
+                            {product.ingredients.map((ingredient) => (
+                                <li key={ingredient}>{ingredient}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </ScrollArea>
             </div>
 
             <Button className="rounded-ful w-full mt-6">Adicionar à sacola</Button>
