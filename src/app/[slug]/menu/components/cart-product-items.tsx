20715857820e78777fb2dc8868b34a8b549e3521
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-02-27 08:57:32 -0300

    feat: ensure cart product quantity can be decreased

--- src/app/[slug]/menu/components/cart-product-items.tsx
+++ src/app/[slug]/menu/components/cart-product-items.tsx
@@ -1,30 +1,18 @@
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/utils/format-currency";
 
-import { CartProduct } from "../contexts/cart";
+import { CartContext, CartProduct } from "../contexts/cart";
 
 interface CartProductItemProps {
     product: CartProduct
 }
 
 const CartProductItem = ({ product }: CartProductItemProps) => {
-    const [quantity, setQuantity] = useState(0);
-    
-    const handleDecreaseQuantity = () => {
-        setQuantity((prev) => {
-            if(prev === 1){
-                return 1
-            }
-            return prev - 1
-        })
-    }
-    const handleIncreaseQuantity = () => {
-        setQuantity(prev => prev + 1)
-    }
+    const { decreaseProductQuantity } = useContext(CartContext)
     
     return (
         <div className="flex items-center justify-between">
@@ -37,9 +25,9 @@ const CartProductItem = ({ product }: CartProductItemProps) => {
                     <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
                     <div className="flex items-center gap-1 justify-between w-[80%]">
                         <div className="flex items-center gap-1">
-                            <Button variant="outline" className="h-7 w-7 rounded-xl" onClick={handleDecreaseQuantity}><MinusIcon /></Button>
+                            <Button variant="outline" className="h-7 w-7 rounded-xl" onClick={() => decreaseProductQuantity(product.id)}><MinusIcon /></Button>
                             <p className="w-4 text-center">{product.quantity}</p>
-                            <Button variant="destructive" className="h-7 w-7 rounded-xl" onClick={handleIncreaseQuantity}><PlusIcon /></Button>
+                            <Button variant="destructive" className="h-7 w-7 rounded-xl"><PlusIcon /></Button>
                         </div>
                         <Button variant="outline" className="h-7 w-7 rounded-xl"><TrashIcon /></Button>
                     </div>
