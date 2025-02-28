74d3c5e842791a710ca6cd6a751ab6f96b5343a3
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-02-28 15:09:44 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 import { removeCpfPunctuation } from "@/utils/validate-cpf";
@@ -59,4 +60,5 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  redirect(`/${input.slug}/orders`)
 };
